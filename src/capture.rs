fec01ec88390b09349db1170e4e075e6b98463f4
Noah Seidl <noah.seidl@stud.plus.ac.at>
2026-03-26 12:30:26 +0100

    Langsamere funktionen mal verschoben TODO threadeed fast dct funktionen für y und cb cr werte

--- src/capture.rs
+++ src/capture.rs
@@ -1,6 +1,6 @@
 use std::{sync::mpsc, time::Instant, vec};
 
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 
 use crate::{fastDct};
 
@@ -25,12 +25,10 @@ pub(crate) struct Capture {
     pub(crate) start: Instant,
     pub(crate) counter: i128,
     pub(crate) ycbcr:(Vec<u8>,Vec<u8>,Vec<u8>),
-    ycbcr_last:(Vec<u8>,Vec<u8>,Vec<u8>),
     pub(crate) width: u32,
     pub(crate) height:u32,
     sender: mpsc::SyncSender<(Vec<u8>,Vec<u8>,Vec<u8>)>,
     pub(crate) second_last : u64,
-    thread_number: u32,
 }
 
 
@@ -43,88 +41,15 @@ impl Capture{
         Ok(Self {
             start: Instant::now(),
             counter: -1,
-            ycbcr_last: (Vec::new(), Vec::new(), Vec::new()),
             width,
             height,
             sender,
             second_last: 0,
-            thread_number: 1,
             ycbcr: (Vec::new(), Vec::new(), Vec::new()) ,
         })
     } 
 
 
-
-
-    pub fn convert_rgbto_yuv(&mut self, pixels: &[u8])
-    {
-        let mut y:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
-        let mut cb:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
-        let mut cr:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
-
-        let mut block_length = 2;
-
-        if self.width == RESULTING_WIDTH {
-            block_length = 1;
-        }
-        
-        let width_difference:f32 = self.width as f32 / RESULTING_WIDTH as f32;
-        let height_difference:f32 = self.height as f32 / RESULTING_HEIGHT as f32;
-
-        let mut pixel_index1 = 0;
-        let mut pixel_index2 = 0;
-
-        let mut x_pos = 0;
-        let mut y_pos = 0; 
-        let mut dest_x:f32 = 0.0;
-        let mut dest_y:f32 = 0.0;
-
-        let mut r_average:u32 = 0;
-        let mut g_average:u32 = 0;
-        let mut b_average:u32 = 0;
-
-        let mut r: u8;
-        let mut g: u8;
-        let mut b: u8;
-
-        for index in 0..RESULTING_RESOLUTION{
-            x_pos = index % RESULTING_WIDTH;
-            y_pos = index / RESULTING_WIDTH;
-
-            dest_x = (x_pos as f32 * width_difference);
-            dest_y = (y_pos as f32 * height_difference);    
-            pixel_index1 = dest_x as u32 + dest_y as u32 * self.width;
-            for i in 0..block_length{
-                for j in 0..block_length{
-                    pixel_index2 =((pixel_index1 + j + (i * self.width)) * 4) as usize;
-                    r_average += pixels[pixel_index2] as u32;
-                    g_average += pixels[pixel_index2 + 1] as u32;
-                    b_average += pixels[pixel_index2 + 2] as u32;
-                }
-            }
-            r = (r_average / (block_length * block_length)) as u8;
-            b = (b_average / (block_length * block_length)) as u8;
-            g = (g_average / (block_length * block_length)) as u8;
-
-            r_average = 0;
-            b_average = 0;
-            g_average = 0;
-
-            y.push(Self::convert_y(r, g, b));
-            
-            if x_pos % 2 == 0 && y_pos % 2 == 0{
-                cb.push(Self::convert_cb(r, g, b));
-                cr.push(Self::convert_cr(r, g, b));
-            }
-        }
-
-
-        println!("cr normal: {:?}", &cr[..64]);
-
-        self.ycbcr = (y,cb,cr);
-    }
-
-  
     pub fn convert_rgbto_yuv_threaded(&mut self, pixels: &[u8])
     {        
         let mut y: Vec<u8> = vec![0u8;RESULTING_RESOLUTION as usize];
@@ -207,6 +132,8 @@ impl Capture{
         self.ycbcr = (y,cb_reduced,cr_reduced);
     }
 
+
+
     fn convert_y(r:u8, g:u8, b:u8) -> u8{
         let y = 0.299  * r as f32 + 0.587 * g as f32 + 0.114 * b as f32;
         y as u8
@@ -220,6 +147,256 @@ impl Capture{
         (0.5*r as f32 - 0.418688*g as f32 - 0.081312*b as f32 + 128.0) as u8
     }
 
+
+    //man müsste noch mehr reihen also horizontale werte einfügen dar sonst nicht durch 8 teilbar 
+    //und somit nicht in blöcke verarbeitbar man müsste auf 544 Reihen ohne extras sind es 540
+    pub(crate) fn linear_to_block_cb_cr(&self,cb:&Vec<u8>, cr:&Vec<u8>)->(Vec<u8>,Vec<u8>){
+    //Schlechte implementierung vielleicht wennman hier einfach 255/2 wert reingibt und len des vecs vergößert für extra reihen
+        let mut cb_block: Vec<u8> = vec![0; cb.len() + 4 * (1920 / 2)];
+        let mut cr_block: Vec<u8> = vec![0; cb.len() + 4 * (1920 / 2)];
+
+        let decreased_width = RESULTING_WIDTH as usize / 2;
+        let decreased_height = RESULTING_HEIGHT as usize / 2;
+
+
+        let blocks_per_row = decreased_width / 8;
+
+        cb_block
+        .par_iter_mut()
+        .zip(cr_block.par_iter_mut())
+        .enumerate()
+        .for_each(|(index, (cb_block, cr_block))|{
+            let block_index = index / 64;
+            let local_index = index % 64;
+
+            let local_x = local_index % 8;
+            let local_y = local_index / 8;
+
+            let block_x = (block_index % blocks_per_row) * 8;
+            let block_y = (block_index / blocks_per_row) *8;
+
+            let src_x = (block_x + local_x).min(decreased_width - 1);
+            let src_y = (block_y + local_y).min(decreased_height - 1);
+
+            let source_index = src_y * decreased_width + src_x;
+
+            *cb_block = cb[source_index];
+            *cr_block = cr[source_index];
+
+        });
+
+
+        (cb_block, cr_block)
+    }
+
+    pub(crate) fn linear_block_fast(&self,pixels:& Vec<u8>) -> Vec<u8> {
+        let block_size = 8;
+        let block_area = block_size * block_size;
+        let blocks_x = RESULTING_WIDTH as usize / block_size;
+        let blocks_y = RESULTING_HEIGHT as usize / block_size;
+
+        let mut vec_block: Vec<u8> = vec![0u8; RESULTING_RESOLUTION as usize];
+
+        for global_y in 0..blocks_y{
+            for global_x in 0..blocks_x{
+                for local_y in 0..block_size{
+                    for local_x in 0..block_size{
+                        let y = global_y * block_size + local_y;
+                        let x = global_x * block_size + local_x;
+
+                        let linear_index = x + y * RESULTING_WIDTH as usize;
+                        let block_index = (global_y * blocks_x + global_x) * block_area + local_y * block_size + local_x;
+
+                        vec_block[block_index] = pixels[linear_index];
+                    }
+                }
+            }
+        }
+
+        vec_block
+    }
+
+
+
+
+    //müsste man auch für cr und cb implementieren
+    pub(crate) fn fast_dct(&self,pixels:&mut Vec<u8>)
+    {
+        let mut dct_vec:Vec<f32> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+
+        println!("Yuv werte REAL:");
+        for i in (0..64).step_by(8){
+            println!("yuv: Werte: {:?}", &pixels[i..i+8]);
+        }
+
+        //Könnte man mit par iter chunk warscheinlich um eineiges schneller machen threaded
+        for block in (0..RESULTING_RESOLUTION as usize).step_by(64){
+            let mut block_f32: Vec<f32> = pixels[block..block + 64]
+                .iter()
+                .map(|&p| p as f32)
+                .collect();
+            fastDct::transform_horizontal(&mut block_f32);
+            fastDct::transform_vertical(&mut block_f32);
+            fastDct::dct_matrix(&mut block_f32);
+            dct_vec.extend_from_slice(&block_f32);
+        }
+
+
+
+        println!("Yuv werte REAL:");
+        for i in (0..64).step_by(8){
+            println!("yuv: Werte: {:?}", &dct_vec[i..i+8]);
+        }
+
+        self.inverse_fast_dct(&mut dct_vec);
+
+    }
+
+
+
+    pub(crate) fn inverse_fast_dct(&self,pixels:&mut Vec<f32>)
+    {
+        let mut dct_vec:Vec<f32> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+
+        //hier durch threading auch schneller möglich
+        for block in (0..RESULTING_RESOLUTION as usize).step_by(64){
+            let mut block_f32: Vec<f32> = pixels[block..block + 64].to_vec();
+            fastDct::inverse_horizontal(&mut block_f32);
+            fastDct::inverse_vertical(&mut block_f32);
+            fastDct::inverse_dct_matrix(&mut block_f32);
+            dct_vec.extend_from_slice(&block_f32);
+        }
+
+        let yuv_dct: Vec<u8> = dct_vec.iter()
+            .map(|&f| f.round() as u8)
+            .collect();
+
+        println!("Inverse DCT:");
+        for i in (0..64).step_by(8){
+            println!("yuv: Werte: {:?}", &yuv_dct[i..i+8]);
+        }
+    }
+
+    //schneller dct mit par iter:
+
+    pub(crate) fn threaded_dct(&self,pixels:&mut Vec<u8>)
+    {
+        let mut dct_vec:Vec<f32> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+
+        println!("Yuv werte REAL:");
+        for i in (0..64).step_by(8){
+            println!("yuv: Werte: {:?}", &pixels[i..i+8]);
+        }
+
+        //Könnte man mit par iter chunk warscheinlich um eineiges schneller machen threaded
+        for block in (0..RESULTING_RESOLUTION as usize).step_by(64){
+            let mut block_f32: Vec<f32> = pixels[block..block + 64]
+                .iter()
+                .map(|&p| p as f32)
+                .collect();
+            fastDct::transform_horizontal(&mut block_f32);
+            fastDct::transform_vertical(&mut block_f32);
+            fastDct::dct_matrix(&mut block_f32);
+            dct_vec.extend_from_slice(&block_f32);
+        }
+
+     
+
+        println!("Yuv werte REAL:");
+        for i in (0..64).step_by(8){
+            println!("yuv: Werte: {:?}", &dct_vec[i..i+8]);
+        }
+
+        self.inverse_fast_dct(&mut dct_vec);
+
+    }
+
+
+
+
+
+
+    pub fn send_ycrcb(&self)
+    {
+        self.sender.send(self.ycbcr.clone()).ok();
+    }
+
+
+    ////////////////////////////////////////////////////////////
+    //Langsame Algos die nichtmehr grbraucht werden vl cleanup
+    ////////////////////////////////////////////////////////////
+
+
+
+    pub fn convert_rgbto_yuv(&mut self, pixels: &[u8])
+    {
+        let mut y:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+        let mut cb:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+        let mut cr:Vec<u8> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
+
+        let mut block_length = 2;
+
+        if self.width == RESULTING_WIDTH {
+            block_length = 1;
+        }
+        
+        let width_difference:f32 = self.width as f32 / RESULTING_WIDTH as f32;
+        let height_difference:f32 = self.height as f32 / RESULTING_HEIGHT as f32;
+
+        let mut pixel_index1 = 0;
+        let mut pixel_index2 = 0;
+
+        let mut x_pos = 0;
+        let mut y_pos = 0; 
+        let mut dest_x:f32 = 0.0;
+        let mut dest_y:f32 = 0.0;
+
+        let mut r_average:u32 = 0;
+        let mut g_average:u32 = 0;
+        let mut b_average:u32 = 0;
+
+        let mut r: u8;
+        let mut g: u8;
+        let mut b: u8;
+
+        for index in 0..RESULTING_RESOLUTION{
+            x_pos = index % RESULTING_WIDTH;
+            y_pos = index / RESULTING_WIDTH;
+
+            dest_x = (x_pos as f32 * width_difference);
+            dest_y = (y_pos as f32 * height_difference);    
+            pixel_index1 = dest_x as u32 + dest_y as u32 * self.width;
+            for i in 0..block_length{
+                for j in 0..block_length{
+                    pixel_index2 =((pixel_index1 + j + (i * self.width)) * 4) as usize;
+                    r_average += pixels[pixel_index2] as u32;
+                    g_average += pixels[pixel_index2 + 1] as u32;
+                    b_average += pixels[pixel_index2 + 2] as u32;
+                }
+            }
+            r = (r_average / (block_length * block_length)) as u8;
+            b = (b_average / (block_length * block_length)) as u8;
+            g = (g_average / (block_length * block_length)) as u8;
+
+            r_average = 0;
+            b_average = 0;
+            g_average = 0;
+
+            y.push(Self::convert_y(r, g, b));
+            
+            if x_pos % 2 == 0 && y_pos % 2 == 0{
+                cb.push(Self::convert_cb(r, g, b));
+                cr.push(Self::convert_cr(r, g, b));
+            }
+        }
+
+
+        println!("cr normal: {:?}", &cr[..64]);
+
+        self.ycbcr = (y,cb,cr);
+    }
+
+  
 
     pub(crate) fn dct_transformation(&self)
     {
@@ -371,135 +548,7 @@ impl Capture{
     }
 
 
-    pub(crate) fn linear_to_block_cb_cr(&self,cb:&Vec<u8>, cr:&Vec<u8>)->(Vec<u8>,Vec<u8>){
-        let mut cb_block: Vec<u8> = vec![0; cb.len()];
-        let mut cr_block: Vec<u8> = vec![0; cb.len()];
 
-        let decreased_width = RESULTING_WIDTH as usize / 2;
-        let decreased_height = RESULTING_HEIGHT as usize / 2;
-
-
-        let blocks_per_row = decreased_width / 8;
-
-        cb_block
-        .par_iter_mut()
-        .zip(cr_block.par_iter_mut())
-        .enumerate()
-        .for_each(|(index, (cb_block, cr_block))|{
-            let block_index = index / 64;
-            let local_index = index % 64;
-
-            let local_x = local_index % 8;
-            let local_y = local_index / 8;
-
-            let block_x = (block_index % blocks_per_row) * 8;
-            let block_y = (block_index / blocks_per_row) *8;
-
-            let src_x = (block_x + local_x).min(decreased_width - 1);
-            let src_y = (block_y + local_y).min(decreased_height - 1);
-
-            let source_index = src_y * decreased_width + src_x;
-
-            *cb_block = cb[source_index];
-            *cr_block = cr[source_index];
-
-        });
-
-
-        (cb_block, cr_block)
-    }
-
-    pub(crate) fn linear_block_fast(&self,pixels:& Vec<u8>) -> Vec<u8> {
-        let block_size = 8;
-        let block_area = block_size * block_size;
-        let blocks_x = RESULTING_WIDTH as usize / block_size;
-        let blocks_y = RESULTING_HEIGHT as usize / block_size;
-
-        let mut vec_block: Vec<u8> = vec![0u8; RESULTING_RESOLUTION as usize];
-
-        for global_y in 0..blocks_y{
-            for global_x in 0..blocks_x{
-                for local_y in 0..block_size{
-                    for local_x in 0..block_size{
-                        let y = global_y * block_size + local_y;
-                        let x = global_x * block_size + local_x;
-
-                        let linear_index = x + y * RESULTING_WIDTH as usize;
-                        let block_index = (global_y * blocks_x + global_x) * block_area + local_y * block_size + local_x;
-
-                        vec_block[block_index] = pixels[linear_index];
-                    }
-                }
-            }
-        }
-
-        vec_block
-    }
-
-
-    pub(crate) fn fast_dct(&self,pixels:&mut Vec<u8>)
-    {
-        let mut dct_vec:Vec<f32> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
-
-        println!("Yuv werte REAL:");
-        for i in (0..64).step_by(8){
-            println!("yuv: Werte: {:?}", &pixels[i..i+8]);
-        }
-
-
-        for block in (0..RESULTING_RESOLUTION as usize).step_by(64){
-            let mut block_f32: Vec<f32> = pixels[block..block + 64]
-                .iter()
-                .map(|&p| p as f32)
-                .collect();
-            fastDct::transform_horizontal(&mut block_f32);
-            fastDct::transform_vertical(&mut block_f32);
-            fastDct::dct_matrix(&mut block_f32);
-            dct_vec.extend_from_slice(&block_f32);
-        }
-
-
-
-        println!("Yuv werte REAL:");
-        for i in (0..64).step_by(8){
-            println!("yuv: Werte: {:?}", &dct_vec[i..i+8]);
-        }
-
-        self.inverse_fast_dct(&mut dct_vec);
-
-    }
-
-
-
-    pub(crate) fn inverse_fast_dct(&self,pixels:&mut Vec<f32>)
-    {
-        let mut dct_vec:Vec<f32> = Vec::with_capacity(RESULTING_RESOLUTION as usize);
-
-        for block in (0..RESULTING_RESOLUTION as usize).step_by(64){
-            let mut block_f32: Vec<f32> = pixels[block..block + 64].to_vec();
-            fastDct::inverse_horizontal(&mut block_f32);
-            fastDct::inverse_vertical(&mut block_f32);
-            fastDct::inverse_dct_matrix(&mut block_f32);
-            dct_vec.extend_from_slice(&block_f32);
-        }
-
-        let yuv_dct: Vec<u8> = dct_vec.iter()
-            .map(|&f| f.round() as u8)
-            .collect();
-
-        println!("Inverse DCT:");
-        for i in (0..64).step_by(8){
-            println!("yuv: Werte: {:?}", &yuv_dct[i..i+8]);
-        }
-    }
-
-
-
-
-    pub fn send_ycrcb(&self)
-    {
-        self.sender.send(self.ycbcr.clone()).ok();
-    }
 
 
 
